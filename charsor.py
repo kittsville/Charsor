1c3da624ff76eb4980450aef2350f852215d1495
Kit <kittsville@googlemail.com>
2015-12-31 23:12:52 +0000

    Added logging of points to CSV
    Added timestamps to file names (to allow multiple runs without
    overwriting each time)

--- charsor.py
+++ charsor.py
@@ -1,5 +1,6 @@
 import  Tkinter
 from    time    import sleep
+from    time    import time
 import  heatmap
 
 SAMPLE_FREQUENCY = 10 # How many times a second to poll the cursor position
@@ -13,7 +14,7 @@ screenHeight = cursor.winfo_screenheight()
 
 print screenWidth, screenHeight
 
-points = []
+cursorPositions = []
 
 # Runs until interupt (Ctrl + C) then generates heatmap on exit
 try:
@@ -23,13 +24,19 @@ try:
         # Flips vertical co-ordinates so heatmap is correct way up
         verticalPos = abs(verticalPos - screenHeight)
         
-        points.append((horizontalPos, verticalPos))
+        cursorPositions.append((horizontalPos, verticalPos))
         
         sleep(0.1)
 except KeyboardInterrupt:
     pass
 
+# Function is only used once so that the heatmap/csv file names don't differ
+currentTime = time()
+
+with open("cursor_%d.csv" % currentTime,'w+') as cursorLogFile:
+    for position in cursorPositions:
+        print >> cursorLogFile, position
 
 hm = heatmap.Heatmap()
-img = hm.heatmap(points, dotsize=15, size=(screenWidth - 1, screenHeight - 1), area=((0, 0), (screenWidth - 1, screenHeight - 1)))
-img.save("classic.png")
+img = hm.heatmap(cursorPositions, dotsize=15, size=(screenWidth - 1, screenHeight - 1), area=((0, 0), (screenWidth - 1, screenHeight - 1)))
+img.save("heatmap_%d.png" % currentTime)
