77a2c2f15687e2a6fbd47a1e78a9b48c3bead5ba
Kit <kittsville@googlemail.com>
2016-01-01 01:03:24 +0000

    Added printing to console of script state

--- charsor.py
+++ charsor.py
@@ -13,7 +13,8 @@ cursor = Tkinter.Tk()
 screenWidth = cursor.winfo_screenwidth()
 screenHeight = cursor.winfo_screenheight()
 
-print screenWidth, screenHeight
+print "Your screen resolution is %dx%d" % (screenWidth, screenHeight)
+print 'Monitoring starts now and ends when you kill the script\n'
 
 cursorPositions = []
 
@@ -37,14 +38,21 @@ timestamp = strftime("%Y-%m-%dT%H%M%S")
 if not os.path.exists('output'):
     os.makedirs('output')
 
+CSVPath = "output/cursor_%s.csv" % timestamp
+MapPath = "output/heatmap_%s.png" % timestamp
+
 # Saves all measured cursor positions to a Comma Separated Value file
 # You can open this in Excel, R or any text editor
-with open("output/cursor_%s.csv" % timestamp,'w+') as cursorLogFile:
+with open(CSVPath,'w+') as cursorLogFile:
     for position in cursorPositions:
         print >> cursorLogFile, str(position[0]) + ',' + str(position[1])
+
+print 'Saved CSV of cursor positions to ' + CSVPath
 
 # Generates heatmap
 # http://jjguy.com/heatmap/
 hm = heatmap.Heatmap()
 img = hm.heatmap(cursorPositions, dotsize=25, scheme='pbj', size=(screenWidth - 1, screenHeight - 1), area=((0, 0), (screenWidth, screenHeight)))
-img.save("output/heatmap_%s.png" % timestamp)
+img.save(MapPath)
+
+print 'Saved heatmap of cursor positions to ' + MapPath
